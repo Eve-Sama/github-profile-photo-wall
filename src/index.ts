6d5c3f5d4e2f8bab341f396d8527eab89bbad84a
Eve <948832626@qq.com>
2020-10-27 00:02:56 +0800

    feat: create valid size of canvas

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,20 @@ const MaxWidth = 224;
 
 const image = new Image();
 image.src = 'https://file.qingflow.com/uploads/file/0a3f4ded-b856-4685-9c35-48fbff5439d3.png';
-image.onload = () => createValidImage(image);
+image.onload = () => {
+  setBeforeImage();
+  createValidImage(image);
+};
+
+function setBeforeImage(): void {
+  const dom = document.querySelector('#before-image') as HTMLImageElement;
+  dom.src = image.src;
+}
 
 /** Copy image to a canvas with valid size */
 function createValidImage(image: HTMLImageElement): void {
   const { width: imageWidth, height: imageHeight } = image;
-  const canvas = document.querySelector('#source-img') as HTMLCanvasElement;
+  const canvas = document.querySelector('#valid-canvas') as HTMLCanvasElement;
   // Calculate the size of the image by 32 times. idk how to calculate it, my girlfriend told me that...
   const maxHeight = imageHeight % 32 === 0 ? imageHeight : 32 * (~~(imageHeight / 32) + 1);
   canvas.width = MaxWidth;
